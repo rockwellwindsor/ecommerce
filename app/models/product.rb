373d50fba42415e2432c27127e2670ac93d96456
Rockwellwindsor <rockwellwindsor@gmail.com>
2017-03-16 19:46:20 -0500

    Development
    Writing out tests
    
    Stopped page 94.
    Changes to be committed:
        modified:   app/assets/stylesheets/products.scss
        modified:   app/models/product.rb
        modified:   test/controllers/products_controller_test.rb
        modified:   test/fixtures/products.yml
        modified:   test/models/product_test.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,17 @@
 class Product < ApplicationRecord
+
+    # Validations
+    validates :title, :description, :image_file, presence: true
+    validates :title, uniqueness: true
+    validates :price, numericality: { greater_than_or_equal_to: 0.51 }
+    validates :image_file, allow_blank: true, format: { 
+        with: %r{\.(gif|jpg|png)\Z}i,
+        message: 'must be a URL for gif, jpg or png image.'
+    }
+
     has_attached_file :image_file, :styles => { :medium => "400x400#", :thumb => "200x200#" }
     validates_attachment :image_file, :presence => true, :content_type => { :content_type => /\Aimage\/.*\Z/ }, :size => { :less_than => 4.megabyte }
 
+    # Enumerators
     enum gender: { neutral: 0 , mens: 1, womens: 2, kids: 3 }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,52 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+    test "product attributes must not be empty" do
+        product = Product.new
+        assert product.invalid?
+        assert product.errors[:title].any?
+        assert product.errors[:description].any?
+        assert product.errors[:image_file_file_name].any?
+        assert product.errors[:gender].any?
+        assert product.errors[:price].any?
+    end
+
+    test "product price must be positive" do
+        product = Product.new(
+            title: "Sweet new product",
+            description: "This is the description",
+            image_file_file_name: "2.jpg",
+            gender: "neutral"
+        )
+
+        product.price = -1
+        assert product.invalid?
+        assert_equal ["must be greater than or equal to 0.51"], 
+        product.errors[:price]
+        product.price = 1
+        assert product.valid?
+    end
+
+    def new_product(image_file_file_name)
+        Product.new(
+            title: "Sweet old product",
+            description: "This is the description",
+            image_file_file_name: image_file_file_name,
+            gender: "neutral"
+        )
+    end
+
+    test "image url" do
+        ok = %w{ fred.gif fred.jpg fred.png FRED.png FRED.jpg http://a.b.c/x/y/z.gif}
+        bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+        ok.each do | name |
+            assert new_product(name).valid?, "#{ name } shouldn't be invalid"
+        end
+
+        bad.each do | name |
+            assert new_product(name).invalid?, "#{ name } shouldn't be valid"
+        end
+    end
 end
